f9737387b93770989a09a8c5f96d41793bba8177
Vanjaramya <134573891+Vanjaramya@users.noreply.github.com>
2025-11-10 12:56:27 +0530

    modified code in app.java

--- app.java
+++ app.java
@@ -1,6 +1,16 @@
-//lets add some data
-public class HelloWorld {
+
+/*lets add second code to first code without pulling it
+*/
+public class VariablesExample {
     public static void main(String[] args) {
+        int age = 30; // Integer variable
+        String name = "Alice"; // String variable
+        double price = 19.99; // Double-precision floating-point variable
+        boolean isActive = true; // Boolean variable
         System.out.println("Hello, World!");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Is Active: " + isActive);
     }
 }
